646bb7f1591a7558afb882e113bc8d52150ef41e
Eduardo Romero <38575968+wichoedu16@users.noreply.github.com>
2025-12-01 17:28:33 -0500

    Agregar configuracion para bdd

--- infrastructure/src/main/java/ec/com/erre/fastfood/infrastructure/commons/security/DataSourceConfig.java
+++ infrastructure/src/main/java/ec/com/erre/fastfood/infrastructure/commons/security/DataSourceConfig.java
@@ -1,14 +1,10 @@
 package ec.com.erre.fastfood.infrastructure.commons.security;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -16,36 +12,32 @@ import com.zaxxer.hikari.HikariDataSource;
 public class DataSourceConfig {
 
 	@Value("${spring.datasource.url}")
-	private String dataSourceUrl;
+	private String url;
+
 	@Value("${spring.datasource.username}")
-	private String dataSourceUsername;
+	private String username;
+
 	@Value("${spring.datasource.password}")
-	private String dataSourcePassword;
-	@Value("${spring.datasource.hikari.maximum-pool-size}")
-	private Integer dataSourcePoolSize;
-	@Value("${spring.datasource.hikari.minimum-idle}")
+	private String password;
+
+	@Value("${spring.datasource.hikari.maximum-pool-size:10}")
+	private Integer maxPoolSize;
+
+	@Value("${spring.datasource.hikari.minimum-idle:2}")
 	private Integer minimumIdle;
-	@Value("${spring.datasource.hikari.idle-timeout}")
+
+	@Value("${spring.datasource.hikari.idle-timeout:30000}")
 	private Integer idleTimeout;
 
 	@Bean
-	DataSource dataSource() {
-		AbstractRoutingDataSource routingDataSource = new UserRoutingDataSource();
-		Map<Object, Object> dataSourceMap = new HashMap<>();
-		routingDataSource.setTargetDataSources(dataSourceMap);
-		routingDataSource.setDefaultTargetDataSource(createHikariDataSource(dataSourceUsername));
-		return routingDataSource;
-	}
-
-	DataSource createHikariDataSource(String username) {
-		HikariDataSource dataSource = new HikariDataSource();
-		dataSource.setJdbcUrl(dataSourceUrl);
-		//dataSource.setUsername(username);
-		//dataSource.setPassword(dataSourcePassword);
-		dataSource.setMaximumPoolSize(dataSourcePoolSize == null ? 1 : dataSourcePoolSize);
-		dataSource.setMinimumIdle(minimumIdle);
-		dataSource.setIdleTimeout(idleTimeout);
-
-		return dataSource;
+	public DataSource dataSource() {
+		HikariDataSource ds = new HikariDataSource();
+		ds.setJdbcUrl(url);
+		ds.setUsername(username);
+		ds.setPassword(password);
+		ds.setMaximumPoolSize(maxPoolSize);
+		ds.setMinimumIdle(minimumIdle);
+		ds.setIdleTimeout(idleTimeout);
+		return ds;
 	}
 }
